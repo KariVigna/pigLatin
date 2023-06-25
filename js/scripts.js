b1d9b7c27e1b94ac89151607115bbd06ffa3d324
Kari Vigna <karivigna24@gmail.com>
2023-06-25 16:08:35 -0700

    third test is passing
    Co-Authored By: Will Jolley
    yeswilljolley@gmail.com

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,12 @@
 function pigLatin(text) {
     if (text.trim().length === 0) {
     return "that's not a word";
-}
+    }
     let vowel = ["a", "e", "i", "o", "u"]
     if (text.slice(0,0) !== vowel) {
         return text.concat("ay");
-    }
+    } else  {
+      return text.slice(1) + text.slice(0,0) + "ay";
+    }     
 }
-// text.slice(1) + 
+// text.slice(1) +  
