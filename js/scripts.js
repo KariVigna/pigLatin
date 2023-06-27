8822a0a39b82167ef3317340419afbfc11e9f464
Kari Vigna <karivigna24@gmail.com>
2023-06-26 19:01:35 -0700

    fourth test passing
    Co-Authored By: Michael Connelly
    micgcon@gmail.com

--- js/scripts.js
+++ js/scripts.js
@@ -3,10 +3,10 @@ function pigLatin(text) {
     return "that's not a word";
     }
     let vowel = ["a", "e", "i", "o", "u"]
+    let cons = ["b", "c", "d", "f", "g", "h", "k", "l", "m", "n", "p", "q", "r", "s", "t", "v", "x", "y", "z"]
     if (text.slice(0,0) !== vowel) {
         return text.concat("ay");
-    } else  {
-      return text.slice(1) + text.slice(0,0) + "ay";
+    } else {
+      return text.slice(1) + text.slice(0,1) + "ay";
     }     
 }
-// text.slice(1) +  
